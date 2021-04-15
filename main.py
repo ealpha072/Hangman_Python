7eac1a05f0701bda0fd1424f155ae01fe4cba508
ealpha072 <ealpha072@gmail.com>
2021-04-15 12:29:46 +0300

    Function for getting number of allowable wrong attempts

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 import sys
 import random
 
+gameWords = ['computer','interesting','guess','introduction','popular','lion','history',
+                'ponder','debate','issues']
+
+#function for asking user number of attempts
+
+def userNumberOfAttempts():
+    numAttempts = input("How many wrong attempts do you want to allow? [1-25]: ")
+    numAttempts  = int(numAttempts)
+    if numAttempts == 0 or numAttempts > 25:
+        print ("The number {0} is not a number between 1 and 25".format(numAttempts))
+    else:
+        return numAttempts
+
+userNumberOfAttempts()
