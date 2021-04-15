a63a31829671f7b1548a00706085a41ffa59ce0e
ealpha072 <ealpha072@gmail.com>
2021-04-15 11:55:39 +0300

    Moved entire code in a function

--- script.py
+++ script.py
@@ -1,46 +1,48 @@
 import random
 
-playerName = input("Enter your name: ")
-print("Hello "+playerName,"welcome to hangman and good luch")
 
-gameWords = ['computer','interesting','guess','introduction','popular','lion','history',
+      
+def playGame():
+    playerName = input("Enter your name: ")
+    print("Hello "+playerName,"welcome to hangman and good luch")
+
+    gameWords = ['computer','interesting','guess','introduction','popular','lion','history',
                 'ponder','debate','issues']
 
-word = random.choice(gameWords)
+    word = random.choice(gameWords)
 
-#holds player guess as a string, this is compared to the word
-playerGuesses=''
+    #holds player guess as a string, this is compared to the word
+    playerGuesses=''
 
-attempts = 10
+    attempts = 10
+    while attempts > 0:
+        failed = 0
+        for letter in word:
+            if letter in playerGuesses:
+                print(letter)
+            else:
+                print("__")
+                failed +=1
 
-while attempts > 0:
-    failed = 0
-    for letter in word:
-        if letter in playerGuesses:
-            print(letter)
-        else:
-            print("__")
-            failed +=1
-
-    if failed == 0:
-        print ("You win")
-        print("The word is: ",word)
-        break
-    
-    playerGuess = input("guess a character: ")
-    playerGuesses+=playerGuess
-
-    if playerGuess== 'exit':
-        break
-
-    if playerGuess not in word:
-        attempts-=1
-        print("Wrong guess")
-        print("You have ",attempts,"more gueses")
-
-        if attempts == 0:
-            print("You loose")
+        if failed == 0:
+            print ("You win")
+            print("The word is: ",word)
+            break
         
+        playerGuess = input("guess a character: ")
+        playerGuesses+=playerGuess
 
+        if playerGuess== 'exit':
+            break
+
+        if playerGuess not in word:
+            attempts-=1
+            print("Wrong guess")
+            print("You have ",attempts,"more gueses")
+
+            if attempts == 0:
+                print("You loose")
+
+playGame()
 
 
