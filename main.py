591d1d2adb482065551d6ed6fa7910f2635db722
ealpha072 <ealpha072@gmail.com>
2021-04-15 13:13:09 +0300

    Function for getting user word length choice

--- main.py
+++ main.py
@@ -2,17 +2,74 @@
 import sys
 import random
 
-gameWords = ['computer','interesting','guess','introduction','popular','lion','history',
-                'ponder','debate','issues']
+gameWords = ['bulti',
+ 'Rauschenberg',
+ 'quizzing',
+ 'stairwell',
+ 'were-jaguar',
+ 'digressing',
+ 'multicore',
+ 'Posadas',
+ 'decolonises',
+ 'quibblingly',
+ 'Lubitsch',
+ 'decahydrate',
+ 'kimberlites',
+ 'disinhibitor',
+ 'Tsinan',
+ 'situates',
+ 'torcher',
+ 'forswearer',
+ 'Eyemouth',
+ 'biennale',
+ 'moralizes',
+ 'stitches',
+ 'chloranthus',
+ 'benzol',
+ 'delimbed',
+ 'vis-a-vis',
+ 'churners',
+ 'supplicates',
+ 'aminobenzoate',
+ 'hybridising',
+ 'dimple',
+ 'meat-eating',
+ 'conformists',
+ 'cartable',
+ 'misdeem',
+ 'impeditive',
+ 'subverting',
+ 'prefigurative',
+ 'artisanal',
+ 'gager',
+ 'extirp',
+ 'damasking',
+ 'altar-herse',
+ 'hobbleshow',
+ 'hatstand',
+ 'bacteriome',
+ 'long-horned',
+ 'checks',
+ 'sodicity',
+ 'horologue']
 
 #function for asking user number of attempts
 
 def userNumberOfAttempts():
     numAttempts = input("How many wrong attempts do you want to allow? [1-25]: ")
     numAttempts  = int(numAttempts)
-    if numAttempts == 0 or numAttempts > 25:
+    if numAttempts <= 0 or numAttempts > 25:
         print ("The number {0} is not a number between 1 and 25".format(numAttempts))
     else:
         return numAttempts
 
-userNumberOfAttempts()
+def minWordLength():
+    wordLength = input("What min word length do you want ? [4-16]: ")
+    wordLength = int(wordLength)
+    if wordLength < 4 or wordLength >16:
+        print("{0} is not a number between 4 and 16".format(wordLength))
+        minWordLength()
+    else:
+        return wordLength
+
+minWordLength()
