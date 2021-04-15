a104b53e9e49e020818af448836e2fb9c69e23ca
ealpha072 <ealpha072@gmail.com>
2021-04-15 11:08:12 +0300

    Deleted old code for new

--- script.py
+++ script.py
@@ -1,33 +1,7 @@
-userName = input("Enter your name: ")
-print("Hello "+userName,"welcome to hangman")
+playerName = input("Enter your name: ")
+print("Hello "+playerName,"welcome to hangman")
 
-gameWord = "edureka"
+word = "better"
+playerGuess = input("Guess any alphabet")
 
-#holds the player's guess
-playerGuesses = ''
 
-#holds number of turns
-turns = 10
-
-while turns > 0:
-    failed = 0
-    for letter in gameWord:
-        if letter in playerGuesses:
-            print(letter)
-        else:
-            print('__')
-            failed +=1
-    if failed == 0:
-        print("You have won")
-        break
-    
-    playerGuess = input("Guess a character")
-    playerGuesses+=playerGuess
-
-    if playerGuess not in gameWord:
-        turns-=1
-        print("Wrong guess")
-
-        print("You have "+turns, "more guesses to go")
-    if turns == 0:
-        print("You loose")
